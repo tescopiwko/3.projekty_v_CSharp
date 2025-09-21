63334b9793683f1dead6b63f2705b61d9530a7bf
Jan Erlebach <erlebachj24@spstrutnov.cz>
2025-09-21 21:12:18 +0200

    Odstranění zbytečné funkce

--- Zacatecni_CSharp_projekty/01_Kamen-Nuzky-Papir/Program.cs
+++ Zacatecni_CSharp_projekty/01_Kamen-Nuzky-Papir/Program.cs
@@ -17,7 +17,27 @@
                 Random random = new Random();
                 string vyberPocitace = vyber[random.Next(vyber.Length)];
 
-                Console.WriteLine(VyhodnoceniHry(vyberCloveka, vyberPocitace));
+                if (vyberCloveka == vyberPocitace)
+                {
+                    Console.WriteLine($"Pocitac vybral: {vyberPocitace}\nRemíza");
+
+                }
+                else if ((vyberCloveka == "kamen" && vyberPocitace == "nuzky") ||
+                       (vyberCloveka == "nuzky" && vyberPocitace == "papir") ||
+                       (vyberCloveka == "papir" && vyberPocitace == "kamen"))
+                {
+                    Console.WriteLine($"Pocitac vybral: {vyberPocitace}\nVyhrál jsi!");
+
+                }
+                else if ((vyberCloveka == "kamen" && vyberPocitace == "papir") ||
+                        (vyberCloveka == "nuzky" && vyberPocitace == "kamen") ||
+                        (vyberCloveka == "papir" && vyberPocitace == "nuzky"))
+                {
+                    Console.WriteLine($"Pocitac vybral: {vyberPocitace}\nProhrál jsi!");
+
+                }
+                else
+                    Console.WriteLine("Neplatný výběr. Zadejte kamen, nuzky nebo papir.");
 
                 // Hrat znovu?
                 Console.Write("Chcete hrát znovu? (ano/ne): ");
@@ -36,30 +56,6 @@
 
         }
 
-        static string VyhodnoceniHry(string vyberCloveka, string vyberPocitace)
-        {
-            if (vyberCloveka == vyberPocitace)
-            {
-                return ($"Pocitac vybral: {vyberPocitace}\nRemíza");
-
-            }
-            else if ((vyberCloveka == "kamen" && vyberPocitace == "nuzky") ||
-                   (vyberCloveka == "nuzky" && vyberPocitace == "papir") ||
-                   (vyberCloveka == "papir" && vyberPocitace == "kamen"))
-            {
-                return ($"Pocitac vybral: {vyberPocitace}\nVyhrál jsi!");
-
-            }
-            else if ((vyberCloveka == "kamen" && vyberPocitace == "papir") ||
-                    (vyberCloveka == "nuzky" && vyberPocitace == "kamen") ||
-                    (vyberCloveka == "papir" && vyberPocitace == "nuzky"))
-            {
-                return ($"Pocitac vybral: {vyberPocitace}\nProhrál jsi!");
-
-            }
-            else
-                return ("Neplatný výběr. Zadejte kamen, nuzky nebo papir.");
-
-        }              
+                  
     }
 }
