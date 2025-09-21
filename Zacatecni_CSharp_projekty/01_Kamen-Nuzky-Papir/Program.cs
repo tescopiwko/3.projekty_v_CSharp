7823793f66b9cd32af5d250d1f53fda00cf9757d
Jan Erlebach <erlebachj24@spstrutnov.cz>
2025-09-21 14:10:02 +0200

    Kámen nůžky papír dokončeno, až na pár drobností -> zadání do hrát znovu

--- Zacatecni_CSharp_projekty/01_Kamen-Nuzky-Papir/Program.cs
+++ Zacatecni_CSharp_projekty/01_Kamen-Nuzky-Papir/Program.cs
@@ -4,16 +4,62 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Vítejte ve hře kámen, nůžky, papír!");
-            Console.WriteLine(" ");
-            Console.Write("Zadejte svůj výběr (kámen, nůžky, papír): ");
-            string userChoice = Console.ReadLine().ToLower();
-            string[] vyber = { "kámen", "nůžky", "papír" };
-            Random random = new Random();
-            string vyberPocitace = vyber[random.Next(vyber.Length)];
+            bool hracChceHrat = true;
 
-            Console.WriteLine($"Pocitac vybral: {vyberPocitace}");
+            Console.WriteLine("Vítejte ve hře kamen, nuzky, papir! (bez diakritiky)");
+
+            while (hracChceHrat)
+            {
+                Console.WriteLine(" ");
+                Console.Write("Zadejte svůj výběr (kamen, nuzky, papir): ");
+                string vyberCloveka = Console.ReadLine().ToLower();
+                string[] vyber = { "kamen", "nuzky", "papir" };
+                Random random = new Random();
+                string vyberPocitace = vyber[random.Next(vyber.Length)];
+
+                Console.WriteLine(VyhodnoceniHry(vyberCloveka, vyberPocitace));
+
+                // Hrat znovu?
+                Console.Write("Chcete hrát znovu? (ano/ne): ");
+                string odpoved = Console.ReadLine().ToLower();
+                if (odpoved != "ano")
+                {
+                    hracChceHrat = false;
+                    Console.WriteLine("Děkuji za hru!");
+                }
+                else
+                {
+                    hracChceHrat = true; Console.WriteLine();
+                }
+
+            }
 
         }
+
+        static string VyhodnoceniHry(string vyberCloveka, string vyberPocitace)
+        {
+            if (vyberCloveka == vyberPocitace)
+            {
+                return ($"Pocitac vybral: {vyberPocitace}\nRemíza");
+
+            }
+            else if ((vyberCloveka == "kamen" && vyberPocitace == "nuzky") ||
+                   (vyberCloveka == "nuzky" && vyberPocitace == "papir") ||
+                   (vyberCloveka == "papir" && vyberPocitace == "kamen"))
+            {
+                return ($"Pocitac vybral: {vyberPocitace}\nVyhrál jsi!");
+
+            }
+            else if ((vyberCloveka == "kamen" && vyberPocitace == "papir") ||
+                    (vyberCloveka == "nuzky" && vyberPocitace == "kamen") ||
+                    (vyberCloveka == "papir" && vyberPocitace == "nuzky"))
+            {
+                return ($"Pocitac vybral: {vyberPocitace}\nProhrál jsi!");
+
+            }
+            else
+                return ("Neplatný výběr. Zadejte kamen, nuzky nebo papir.");
+
+        }              
     }
 }
