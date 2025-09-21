471a013d688a48c1c1f71dd99f21afa4e8ff0185
Jan Erlebach <erlebachj24@spstrutnov.cz>
2025-09-21 21:49:05 +0200

    Pouze přidání složek obou zbylích projektů na kroužek

---
+++ Zacatecni_CSharp_projekty/02_Piskvorky/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _02_Piskvorky
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ Zacatecni_CSharp_projekty/03_Sibenice/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _03_Sibenice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
