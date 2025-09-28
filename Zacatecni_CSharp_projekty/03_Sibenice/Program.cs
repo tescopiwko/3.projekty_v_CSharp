f536ee2679c6477d3a7f958e34a3f529cfcb9e8f
Jan Erlebach <erlebachj24@spstrutnov.cz>
2025-09-28 13:13:03 +0200

    Základy šibenice - podle počtu slov vytvoří podtržítka

--- Zacatecni_CSharp_projekty/03_Sibenice/Program.cs
+++ Zacatecni_CSharp_projekty/03_Sibenice/Program.cs
@@ -4,7 +4,21 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            NactiSlovaZListu();
+            List<string> slovaDoSibenice = NactiSlovaZListu();
+            Random random = new Random();
+            string vybraneSlovo = slovaDoSibenice[random.Next(slovaDoSibenice.Count)];
+            //Console.WriteLine(vybraneSlovo);
+            string skryteSlovo = new string('_', vybraneSlovo.Length);
+            Console.WriteLine(skryteSlovo);
+
         }
-    }
+
+        static List<string> NactiSlovaZListu()
+        {
+            List<string> slovaDoSibenice = File.ReadLines("Slova.txt").ToList();
+            return slovaDoSibenice;
+        }
+        
+    }   
 }
