5627de4ebb562dde4b84490c58377911f7a6c0e5
Jan Erlebach <erlebachj24@spstrutnov.cz>
2025-10-04 16:35:42 +0200

    Projekt Chcete být milionářem hotov

--- Zacatecni_CSharp_projekty/NudneTextoveChceteBytMilionarem/Program.cs
+++ Zacatecni_CSharp_projekty/NudneTextoveChceteBytMilionarem/Program.cs
@@ -4,35 +4,75 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
             bool beziHra = true;
             bool jeOdpovedSpravna = false;
             int pomocnyIndex = 0;
             int konecneKolo = 3;
-            int konto = 0;
+            string konto = "";
             List<string> otazkyDoHry = nacteniZeSouboru("Otazky.txt");
             List<string> odpovediDoHry = nacteniZeSouboru("Odpovedi.txt");
+            List<string> vyhraPodleKola = nacteniZeSouboru("Odmeny.txt");
+            Console.WriteLine("Vítej ve variantě hry Chcete být milionářem zaměřené výhradně na originální Star Wars trilogii!!!");
+            Console.Write("""
+                               ____ _              _         _       __  _                               
+                              / ___| |__   ___ ___| |_ ___  | |__  _/_/_| |_                             
+                             | |   | '_ \ / __/ _ \ __/ _ \ | '_ \| | | | __|                            
+                             | |___| | | | (_|  __/ ||  __/ | |_) | |_| | |_                             
+                              \____|_| |_|\___\___|\__\___| |_.__/ \__, |\__|                            
+                                        _ _ _               __  \\//___/              __   _             
+                              _ __ ___ (_) (_) ___  _ __   /_/_ _\/_ ___ _ __ ___    / /__| |_ __ _ _ __ 
+                             | '_ ` _ \| | | |/ _ \| '_ \ / _` | '__/ _ \ '_ ` _ \  | / __| __/ _` | '__|
+                             | | | | | | | | | (_) | | | | (_| | | |  __/ | | | | | | \__ \ || (_| | |   
+                             |_| |_| |_|_|_|_|\___/|_| |_|\__,_|_|_ \___|_| |_| |_| | |___/\__\__,_|_|   
+                             __      ____ _ _ __ ___    ___  _ __(_) __ _(_)_ __   __\_\ |_ __ (_)       
+                             \ \ /\ / / _` | '__/ __|  / _ \| '__| |/ _` | | '_ \ / _` | | '_ \| |       
+                              \ V  V / (_| | |  \__ \ | (_) | |  | | (_| | | | | | (_| | | | | | |       
+                               \_/\_/ \__,_|_|  |___/  \___/|_|  |_|\__, |_|_| |_|\__,_|_|_| |_|_|       
+                              _        _ _             _            |___/_ _        __                   
+                             | |_ _ __(_) | ___   __ _(_) ___    ___  __| (_) ___ __\ \                  
+                             | __| '__| | |/ _ \ / _` | |/ _ \  / _ \/ _` | |/ __/ _ \ |                 
+                             | |_| |  | | | (_) | (_| | |  __/ |  __/ (_| | | (_|  __/ |                 
+                              \__|_|  |_|_|\___/ \__, |_|\___|  \___|\__,_|_|\___\___| |                 
+                                                 |___/                              /_/     
+                           """);
+            Console.WriteLine();
+            Console.WriteLine($"Pravidla jsou jednoduchá, na každou otázku je pouze jedna správná odpověď, pokud odpovíš špatně hra končí a odnášíš si výhru z posledního správně zodpovězeného kola, pokud odpovíš správně postupuješ do dalšího kola, pokud zodpovíš všechny otázky správně vyhráváš maximum, které je: {vyhraPodleKola[vyhraPodleKola.Count-1]} místní-měny.");
+            Console.ReadLine();
+            Console.Clear();
+
             while (beziHra)
             {
-                Console.Write($"Otázka číslo {pomocnyIndex+1}. {otazkyDoHry[pomocnyIndex]} ");
-                string odpoved = Console.ReadLine();
-                if (odpoved == odpovediDoHry[pomocnyIndex])
+                Console.Write($"Otázka číslo {pomocnyIndex + 1}. za {vyhraPodleKola[pomocnyIndex]} {otazkyDoHry[pomocnyIndex]} ");
+                string odpoved = Console.ReadLine().ToLower();
+                if (odpoved == odpovediDoHry[pomocnyIndex].ToLower())
                 {
-                    konto = konto + (pomocnyIndex + 1) * 500;
-                    Console.WriteLine($"Výborně odpověděl jsi správně na {pomocnyIndex + 1}. otázku odnášíš si: {konto} místní-měny");
+                    konto = vyhraPodleKola[pomocnyIndex];
                     pomocnyIndex++;
-                    
+                    if (pomocnyIndex >= otazkyDoHry.Count)
+                    {
+                        Console.WriteLine($"Gratuluji odnášíš si maximální výhru a tím je {konto} místní-měny!");
+                        Console.Write("""
+                         _  ___         ___   ___   ___         ___   ___   ___  _   _   _ 
+                        / |/ _ \       / _ \ / _ \ / _ \       / _ \ / _ \ / _ \| | | | | |
+                        | | | | |     | | | | | | | | | |     | | | | | | | | | | | | | | |
+                        | | |_| |  _  | |_| | |_| | |_| |  _  | |_| | |_| | |_| |_| |_| |_|
+                        |_|\___/  (_)  \___/ \___/ \___/  (_)  \___/ \___/ \___/(_) (_) (_)
+                                                               
+                        """);
+                        break;
+                    }
+                    Console.WriteLine($"Výborně možnost {odpovediDoHry[pomocnyIndex-1]} byla správnou odpovědí na {pomocnyIndex + 1}. otázku, odnášíš si: {konto} místní-měny");
+                    Console.WriteLine("Stiskněte Enter pro zobrzení další otázku");
+                    Console.ReadLine();
+                    Console.Clear();
                 }
-
-                
-
+                else
+                {
+                    Console.WriteLine($"Bohužel možnost {odpoved} nebyla správnou odpovědí na {pomocnyIndex + 1}. otázku, správná odpověď byla {odpovediDoHry[pomocnyIndex]}, odnášíš si: {konto} místní-měny");
+                    break;
+                }
             }
-
-            
-            
-
         }
-
         static List<string> nacteniZeSouboru(string otazkyNeboOdpovedi)
         {
             List<string> otazkyDoHry = File.ReadLines(otazkyNeboOdpovedi).ToList();
