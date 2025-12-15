627a87497017827f53afcac86e8a5e62e41bd796
Jan Erlebach <erlebachj24@spstrutnov.cz>
2025-12-15 17:00:27 +0100

    Uhodni Cislo projekt - dokoncen

---
+++ Zacatecni_CSharp_projekty/06_UhodniCislo/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace _06_UhodniCislo
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int cislo = random.Next(1, 101);
+            Console.WriteLine(cislo);
+
+            Console.Write("Hádejte číslo: ");
+            int Zvolenecislo = Convert.ToInt32(Console.ReadLine());
+
+            while (Zvolenecislo != cislo)
+            {
+                if (Zvolenecislo > cislo)
+                {
+                    Console.WriteLine("Cislo je prilis velke. Zkus mensi");
+                }
+                else
+                {
+                    Console.WriteLine("Cislo je prilis male. Zkus vetsi");
+                }
+                Console.WriteLine("Hádejte číslo: ");
+                Zvolenecislo = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine("Uhodl jsi číslo!");
+        }
+    }
+}
